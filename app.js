d7b8e7b39fc59b7a80d6a018f087a166b7a23b20
mathia-gowb <Tobvins@gmail.com>
2022-04-08 09:57:35 +0200

    added event listeners for gettting messages

--- app.js
+++ app.js
@@ -23,11 +23,40 @@ const io=new Server(server,{
     }
 });
 io.on("connection",(socket)=>{
-    console.log("connected")
-    socket.on('NEW_MESSAGE',(data)=>{
-        //add the new data to database then when successful reload the 
-        console.log(data)
+    /* this will be called when a new user initiate a chart */
+    socket.on('NEW_CHAT',(data)=>{
+        //add the message to the database
+       const user=data.name;
+       /* check if the current user exists */
+       const message=new messages({
+           clientName:user,
+           messages:[{
+               messageContent:data.message,
+               sender:{
+                   senderName:user,
+                   admin:false,
+               }
+           }]
+       })
+       message.save()
+       .then(data=>{
+           /* the new message event will provide the chat id to the front end and the event will cal the get messages which will return all the messages in that chat */
+            io.emit('NEW_MESSAGE',{chatId:data._id});
+            io.emit('INITIATE_PRIVATE_CHART',{chatId:data._id})
+       })
+
     })
+    socket.on('GET_MESSAGES',(data)=>{
+        //the GET_MESSAGES  is fired and it gets all the messages using the id of the prop
+        messages.findById(data.chatId)
+        .then(chat=>{
+            console.log('this is the current chat...........................')
+            console.log(chat)
+        })
+    })
+      //return the message object
+      //send the instruction to reload the charts list on admin(set users id to object id) & to redirect the user(set id to object id) to /messages
+
 })
 app.get('/api',(req,res)=>{
     //access the database
@@ -37,23 +66,27 @@ app.get('/api',(req,res)=>{
 app.post('/admin',(req,res)=>{
     res.json({"response":"you made request as admin"})
 })
-app.post('/public',(req,res)=>{
-    const user=req.query.name;
-    /* check if the current user exists */
-    const message=new messages({
-        clientName:user,
-        messages:[{
-            messageContent:req.query.message,
-            sender:{
-                senderName:user,
-                admin:false,
-            }
-        }]
-    })
-    message.save()
-    .then(data=>{
-        res.json({"response":"ngelekanyo you made request to public",redirect:"/public/messages"})
-    })
-    /* fetch all their message and return them in json with the newuser of false */
-})
+//app.post('/newuser',(req,res)=>{
+//    console.log(req)
+//    const user=req.query.name;
+//    /* check if the current user exists */
+//    const message=new messages({
+//        clientName:user,
+//        clientId:req.query.sessionId,
+//        messages:[{
+//            messageContent:req.query.message,
+//            sender:{
+//                senderName:user,
+//                admin:false,
+//            }
+//        }]
+//    })
+//    message.save()
+//    .then(data=>{
+//        res.json({"response":"ngelekanyo you made request to public",redirect:"/public/messages"});
+//       // io.emit('RECEIVE_MESSAGE',{newMessage:true})
+//    })
+//    /* fetch all their message and return them in json with the newuser of false */
+//})
+
 server.listen(5000,()=>{console.log("Server started on port 5000")})
